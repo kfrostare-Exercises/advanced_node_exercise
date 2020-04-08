f6e14a27f35489b5aa53c580b4fedc451ebcdcb9
karolina <karolina.frostare@gmail.com>
2020-04-08 13:02:55 +0200

    migrates sequelizer db

---
+++ database/config/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  "development": {
+    "username": "karro",
+    "password": "password",
+    "database": "books_development",
+    "host": "127.0.0.1",
+    "dialect": "postgres",
+    "operatorsAliases": false
+  },
+  "test": {
+    "username": "karro",
+    "password": "password",
+    "database": "books_test",
+    "host": "127.0.0.1",
+    "dialect": "postgres",
+    "operatorsAliases": false
+  },
+  "production": {
+    "username": "karro",
+    "password": "password",
+    "database": "books_production",
+    "host": "127.0.0.1",
+    "dialect": "postgres",
+    "operatorsAliases": false
+  }
+}
